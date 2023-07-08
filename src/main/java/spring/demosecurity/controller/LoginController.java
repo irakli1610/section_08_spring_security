fba5cdff7df3f471aad01ce403b89bf3c5fdec83
comp_theme <iraklitsulukidze14@gmail.com>
2023-07-08 19:13:58 +0400

    initial commit, spting security, section 8

---
+++ src/main/java/spring/demosecurity/controller/LoginController.java
@@ -0,0 +1,17 @@
+package spring.demosecurity.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+
+    @GetMapping("/showMyLoginPage")
+    public String showLoginPage(){
+        return "fancy-login";
+    }
+    @GetMapping("/access-denied")
+    public String showAccessDeniedPage(){
+        return "access-denied";
+    }
+}
